051200b8a70bb6ca4ad43614b9f4e72c425465c9
nic <netzeln@gmail.com>
2016-02-12 08:40:33 -0800

    create input form

--- src/Contact.php
+++ src/Contact.php
@@ -3,8 +3,22 @@ class Contact {
     private $firstname;
     private $lastname;
     private $phone;
-    private $address;
+    private $street;
+    private $city;
+    private $state;
     private $email;
+
+    function __construct($firstname, $lastname, $phone, $street, $city,
+    $state, $email)
+    {
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->phone = $phone;
+        $this->street = $street;
+        $this->city = $city;
+        $this->state = $state;
+        $this->email = $email;
+    }
 
     function setFirst($newFirst)
     {
@@ -20,7 +34,6 @@ class Contact {
     {
         $this->lastname = $newLast;
     }
-
     function getLast()
     {
         return $this->lastname;
@@ -30,29 +43,50 @@ class Contact {
     {
         $this->phone = $newPhone;
     }
-
     function getPhone()
     {
         return $this->phone;
     }
 
-    function setAddress($newAddress)
+    function setStreet($newStreet)
     {
-        $this->address = $newAddress;
+        $this->street = $newStreet;
+    }
+    function getStreet()
+    {
+        return $this->street;
     }
 
-    function getAddress()
+    function setCity($newCity)
     {
-        return $this->address;
+        $this->city = $newCity;
+    }
+    function getCity()
+    {
+        return $this->city;
+    }
+
+    function setState($newState)
+    {
+        $this->state = $newState;
+    }
+    function getState()
+    {
+        return $this->city;
     }
 
     function setEmail($newEmail)
     {
         $this->email = $newEmail;
     }
-
     function getEmail()
     {
         return $this->email;
     }
+
+    function fullName()
+    {
+        return $this->firstname . " " . $this->lastname;
+    }
+
 } ?>
